d908f8049625aa9b0321223d1d5e69287631e9f9
Abijith B <abijith.b@keyvalue.systems>
2022-12-26 21:49:48 +0530

    Lsp keybindings re-map edit

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -22,19 +22,21 @@ lsp.setup_nvim_cmp({
 	mapping = cmp_mappings
 });
 
+local builtin = require('telescope.builtin');
+
 -- some more keymaps
 lsp.on_attach(function(client, bufnr)
 	-- do stuff if lsp exist or fallback to vim default behaviour
 	local opts = { buffer = bufnr, remap = false }
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts);
+	vim.keymap.set("n", "gd", builtin.lsp_definitions, opts);
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts);
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts);
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts);
 	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts);
 	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts);
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts);
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts);
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts);
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts);
+	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts);
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts);
 	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts);
 end);
 lsp.setup()
