bdd87585ce1e045cb5aeb2ccb681792ba80052d9
Abijith B <abijith.b@keyvalue.systems>
2023-12-10 20:34:52 +0530

    Add lsp configurations

---
+++ nvim/lua/abijith/lazy/init.lua
@@ -0,0 +1,25 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	'theprimeagen/harpoon',
+	'mbbill/undotree',
+	'tpope/vim-fugitive',
+	'tpope/vim-sleuth',
+	'junegunn/goyo.vim',
+	'junegunn/limelight.vim',
+	{ 
+		'rose-pine/neovim',
+		name = 'rose-pine'
+	},
+	{
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
+		dependencies = { { 'nvim-lua/plenary.nvim' } }
+	},
+}
